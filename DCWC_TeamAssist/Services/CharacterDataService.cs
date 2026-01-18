242d99ce3fe5919707046275508de7927af08172
Quinn Turner <Quinn.Turner261298@gmail.com>
2026-01-18 21:58:21 +0000

    Add all 59 DC Worlds Collide characters matching portrait filenames

--- DCWC_TeamAssist/Services/CharacterDataService.cs
+++ DCWC_TeamAssist/Services/CharacterDataService.cs
@@ -4,7 +4,6 @@ namespace DCWC_TeamAssist.Services;
 
 public class CharacterDataService
 {
-    // This will store all available characters in the game
     private List<Character> _allCharacters = new();
 
     public CharacterDataService()
@@ -14,114 +13,68 @@ public class CharacterDataService
 
     private void InitializeCharacters()
     {
-        // TODO: Add actual DC World's Collide characters
-        // For now, adding sample characters
+        // DC World's Collide Characters - IDs match portrait filenames
         _allCharacters = new List<Character>
         {
-            new Character
-            {
-                Id = "batman",
-                Name = "Batman",
-                Rarity = CharacterRarity.Legendary,
-                Role = CharacterRole.Tank,
-                Power = 5000,
-                Health = 8000,
-                Attack = 1200,
-                Defense = 900,
-                ImageUrl = "/images/batman.png",
-                Abilities = new List<string> { "Dark Knight", "Batarang", "Grapple Hook" }
-            },
-            new Character
-            {
-                Id = "superman",
-                Name = "Superman",
-                Rarity = CharacterRarity.Legendary,
-                Role = CharacterRole.DPS,
-                Power = 5500,
-                Health = 7500,
-                Attack = 1500,
-                Defense = 800,
-                ImageUrl = "/images/superman.png",
-                Abilities = new List<string> { "Heat Vision", "Super Strength", "Flight" }
-            },
-            new Character
-            {
-                Id = "wonderwoman",
-                Name = "Wonder Woman",
-                Rarity = CharacterRarity.Legendary,
-                Role = CharacterRole.Tank,
-                Power = 5200,
-                Health = 7800,
-                Attack = 1300,
-                Defense = 950,
-                ImageUrl = "/images/wonderwoman.png",
-                Abilities = new List<string> { "Lasso of Truth", "Shield Block", "Godly Strike" }
-            },
-            new Character
-            {
-                Id = "flash",
-                Name = "The Flash",
-                Rarity = CharacterRarity.Legendary,
-                Role = CharacterRole.DPS,
-                Power = 5100,
-                Health = 6500,
-                Attack = 1600,
-                Defense = 600,
-                ImageUrl = "/images/flash.png",
-                Abilities = new List<string> { "Speed Force", "Lightning Punch", "Time Remnant" }
-            },
-            new Character
-            {
-                Id = "greenlantern",
-                Name = "Green Lantern",
-                Rarity = CharacterRarity.Epic,
-                Role = CharacterRole.Support,
-                Power = 4200,
-                Health = 6800,
-                Attack = 1100,
-                Defense = 750,
-                ImageUrl = "/images/greenlantern.png",
-                Abilities = new List<string> { "Power Ring", "Construct Shield", "Energy Blast" }
-            },
-            new Character
-            {
-                Id = "aquaman",
-                Name = "Aquaman",
-                Rarity = CharacterRarity.Epic,
-                Role = CharacterRole.Tank,
-                Power = 4500,
-                Health = 7200,
-                Attack = 1000,
-                Defense = 850,
-                ImageUrl = "/images/aquaman.png",
-                Abilities = new List<string> { "Trident Strike", "Tidal Wave", "Telepathy" }
-            },
-            new Character
-            {
-                Id = "cyborg",
-                Name = "Cyborg",
-                Rarity = CharacterRarity.Epic,
-                Role = CharacterRole.DPS,
-                Power = 4300,
-                Health = 6500,
-                Attack = 1250,
-                Defense = 700,
-                ImageUrl = "/images/cyborg.png",
-                Abilities = new List<string> { "Sonic Cannon", "Tech Upgrade", "Grid Connection" }
-            },
-            new Character
-            {
-                Id = "harleyquinn",
-                Name = "Harley Quinn",
-                Rarity = CharacterRarity.Epic,
-                Role = CharacterRole.DPS,
-                Power = 4100,
-                Health = 6000,
-                Attack = 1350,
-                Defense = 550,
-                ImageUrl = "/images/harleyquinn.png",
-                Abilities = new List<string> { "Mallet Smash", "Explosive Surprise", "Acrobatic Dodge" }
-            }
+            new Character { Id = "aquaman", Name = "Aquaman", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Tank },
+            new Character { Id = "batman", Name = "Batman", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "bizarro", Name = "Bizarro", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "black-canary", Name = "Black Canary", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "black-manta-", Name = "Black Manta", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "bloodsport", Name = "Bloodsport", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "blue-bettle-", Name = "Blue Beetle", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "captain-boomerang-1", Name = "Captain Boomerang", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "captain-cold-", Name = "Captain Cold", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "catwoman", Name = "Catwoman", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "clay-face", Name = "Clayface", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "constantine-", Name = "Constantine", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "cyborg-", Name = "Cyborg", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "deadshot", Name = "Deadshot", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "deathstroke-", Name = "Deathstroke", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "doctor-fate", Name = "Doctor Fate", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "doctor-phosphorus-", Name = "Doctor Phosphorus", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "frankenstein", Name = "Frankenstein", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "gorilla-", Name = "Gorilla Grodd", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Tank },
+            new Character { Id = "green-arrow", Name = "Green Arrow", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "green-lantern-hal-", Name = "Green Lantern (Hal Jordan)", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "green-lantern-jessica", Name = "Green Lantern (Jessica Cruz)", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "green-lantern-john-", Name = "Green Lantern (John Stewart)", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "harley-quinn", Name = "Harley Quinn", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "huntress", Name = "Huntress", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "kid-flash-", Name = "Kid Flash", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "killer-croc", Name = "Killer Croc", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "king-shark-", Name = "King Shark", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "lex-luther", Name = "Lex Luthor", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "martian-", Name = "Martian Manhunter", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Tank },
+            new Character { Id = "mera", Name = "Mera", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "mirror-master", Name = "Mirror Master", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "mr-freeze", Name = "Mr. Freeze", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "nightwing", Name = "Nightwing", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "ocean-master", Name = "Ocean Master", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "pandorra", Name = "Pandora", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "phantom-stranger", Name = "Phantom Stranger", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "poison-ivy-", Name = "Poison Ivy", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "raven-", Name = "Raven", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support },
+            new Character { Id = "red-hood", Name = "Red Hood", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "red-robin-", Name = "Red Robin", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "robin-", Name = "Robin", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "scarecrow", Name = "Scarecrow", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "sinestro-", Name = "Sinestro", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "star-sapphire", Name = "Star Sapphire", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "starfire", Name = "Starfire", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "superboy", Name = "Superboy", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "supergirl-", Name = "Supergirl", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "superman", Name = "Superman", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Tank },
+            new Character { Id = "the-bridge-", Name = "The Bride", Rarity = CharacterRarity.Epic, Role = CharacterRole.Tank },
+            new Character { Id = "the-cheetah", Name = "The Cheetah", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "the-flash-", Name = "The Flash", Rarity = CharacterRarity.Legendary, Role = CharacterRole.DPS },
+            new Character { Id = "the-penguin-", Name = "The Penguin", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "the-question", Name = "The Question", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "two-face", Name = "Two-Face", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "vixen", Name = "Vixen", Rarity = CharacterRarity.Epic, Role = CharacterRole.DPS },
+            new Character { Id = "weasel-", Name = "Weasel", Rarity = CharacterRarity.Epic, Role = CharacterRole.Support },
+            new Character { Id = "wonder-woman", Name = "Wonder Woman", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Tank },
+            new Character { Id = "zatanna", Name = "Zatanna", Rarity = CharacterRarity.Legendary, Role = CharacterRole.Support }
         };
     }
 
